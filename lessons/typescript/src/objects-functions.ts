499728d7c3c3a3358299ef432becc45279df8ec5
ryan <concepting@protonmail.com>
2024-05-13 20:51:20 -0400

    finished objects and functions

--- lessons/typescript/src/objects-functions.ts
+++ lessons/typescript/src/objects-functions.ts
@@ -122,3 +122,23 @@ processInput('Hello');
 function createEmployee({ id }: { id:number }):{id:number; isActive:boolean}{
     return {id, isActive:id % 2 === 0};
 };
+
+const first = createEmployee({id:1});
+const second = createEmployee({id:2});
+console.log(first, second);
+
+// alternative
+
+function createStudent(student:{id:number, name:string}):void{
+    console.log(`Welcome to the course ${student.name.toUpperCase()}!!!`);
+}
+
+const newStudent = {
+    id: 5,
+    name: 'anna',
+    email: 'anna@gmail.com'
+}
+
+createStudent(newStudent);
+// createStudent({id: 1, name: 'bob', email: 'bobo@gmail.com'});
+

534af99bd59265ebbb36b8f0518d547015e75486
ryan <concepting@protonmail.com>
2024-05-14 14:53:47 -0400

    updated ReadMe.md and started learning type alias

---
+++ lessons/typescript/src/alias-interface.ts
@@ -0,0 +1,40 @@
+type User = { id: number; name: string; isActive: boolean }
+
+const john: User = {
+    id: 1,
+    name: 'john',
+    isActive: true,
+  };
+  const susan: User = {
+    id: 1,
+    name: 'susan',
+    isActive: false,
+  };
+  
+  function createUser(user: { id: number; name: string; isActive: boolean }): {
+    id: number;
+    name: string;
+    isActive: boolean;
+  } {
+    console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+  
+    return user;
+  }
+
+  type StringOrNumber = string | number;
+
+  let value:StringOrNumber = 'hello';
+  value = 10;
+
+  type Theme = 'light' | 'dark';
+
+  let theme: Theme;
+  theme = 'light';
+  theme = 'dark';
+
+  function setTheme(t: Theme){
+    theme = t;
+
+  }
+
+  setTheme('dark');

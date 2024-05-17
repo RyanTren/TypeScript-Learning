2a56ba0a20a65773281b795327dd070cc3b4b97c
ryan <concepting@protonmail.com>
2024-05-17 17:42:13 -0400

    tupes and enums finished onto type guards

--- folder/lessons/tuples-and-enums.ts
+++ folder/lessons/tuples-and-enums.ts
@@ -123,3 +123,118 @@
 //   });
   
 //   console.log(user);
+
+let someValue: any = 'this is a string';
+let strLength:number = (someValue as string).length;
+
+type Bird = {
+    name: string;
+};
+
+let birdString = '{"name": "Eagle"}';
+let dogString = '{"breed": "Poodle"}';
+
+let birdObject = JSON.parse(birdString);
+let dogObject = JSON.parse(dogString);
+
+let bird = birdObject as Bird; //Eagle
+let dog = dogObject as Bird; //Undefined
+console.log(bird.name + dog.name);
+
+
+enum Status{
+    Pending = 'pending',
+    Declined  = 'declined',
+}
+
+type User = {
+    name: string;
+    status: Status;
+};
+
+/*
+Senario
+
+save Status.Pending in the DB as a String and retrieve string
+from the DB
+*/
+
+const statusValue = 'pending';
+
+const user:User = {name: 'john', status: statusValue as Status};
+
+// Type - 'unknown'
+
+let unknownValue: unknown;
+
+unknownValue = 'string'; //this works
+unknownValue = [1, 2, 3]; //this works
+unknownValue = 42.3344556; //this works
+
+// unknownValue.toFixed(2); //this will not work
+
+// another example
+if (typeof unknownValue === 'number') {
+    console.log(unknownValue.toFixed(2)); // now this works because ts knows that unknownValue is a string in this block
+}
+
+
+function runSomeCode(){
+    const random = Math.random();
+    if(random < 0.5){
+        throw new Error('This is an error');
+    } else{
+        throw 'some error';
+    }
+}
+
+try {
+    runSomeCode();
+} catch (error) {
+    if(error instanceof Error){
+        console.log(error.message);
+    } else{
+        console.log(error);
+    }
+}
+
+// let someValue:never = 0; can't assign anything
+
+type Theme = 'light' | 'dark';
+
+function checkTheme(theme: Theme){
+    if(theme === 'light'){
+        console.log('light theme');
+        return;
+    }
+    if(theme === 'dark'){
+        console.log('dark theme');
+        return;
+    }
+    theme;
+}
+
+enum Color {
+    Red,
+    Blue,
+    // Green,
+}
+
+function getColorName(color: Color){
+    switch(color){
+        case Color.Red:
+            return 'Red';
+        case Color.Blue:
+            return 'Blue';
+        default:
+            // at build time
+            let unexpectedColor:never = color;
+            // at runtime
+            throw new Error (`Unhandled color: ${color}`)
+    }
+}
+
+console.log(getColorName(Color.Red)); // Red
+console.log(getColorName(Color.Blue)); // Blue
+// console.log(getColorName(Color.Green)); // undefined (no case handled in switch)
+

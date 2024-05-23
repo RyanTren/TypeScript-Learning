e14342889ee7083112205f9eabe00e275005436d
ryan <concepting@protonmail.com>
2024-05-22 23:10:26 -0400

    finished tasks

--- folder/typescript/src/tasks.ts
+++ folder/typescript/src/tasks.ts
@@ -13,7 +13,8 @@ type Task = {
     isCompleted: boolean;
 }
 
-const tasks: Task[] = [];
+  // Retrieve tasks from localStorage
+  const tasks: Task[] = loadTasks();
 
 function createTask(event: SubmitEvent){
     event.preventDefault();
@@ -38,24 +39,57 @@ function createTask(event: SubmitEvent){
 taskForm?.addEventListener('submit', (event) => {
     event.preventDefault();
     const taskDescription = formInput?.value;
+    if (taskDescription) {
+      const task: Task = {
+        description: taskDescription,
+        isCompleted: false,
+      };
+      // add task to list
+      addTask(task);
+      // render tasks
+      renderTask(task);
+      // update local storage
+      updateStorage();
 
-    if(taskDescription){
-        const task: Task = {
-            description: taskDescription,
-            isCompleted: false,
-        };
-
-        // add task to list
-        // render tasks
-        // update local storage
-
-        formInput.value = '';
-        return;
+      formInput.value = '';
+      return;
     }
-    alert('Please enter a task description.');
-});
-
-function addTask(task: Task):void{
+    alert('Please enter a task description');
+  });
+  
+  function addTask(task: Task): void {
     tasks.push(task);
     console.log(tasks);
+  }
+
+  function renderTask(task: Task): void {
+    const taskElement = document.createElement('li');
+    taskElement.textContent = task.description;
+    // checkbox
+    const taskCheckbox = document.createElement('input');
+    taskCheckbox.type = 'checkbox';
+    taskCheckbox.checked = task.isCompleted;
+    // toggle checkbox
+    taskCheckbox.addEventListener('change', () => {
+      task.isCompleted = !task.isCompleted;
+      updateStorage();
+    });
+  
+    taskElement.appendChild(taskCheckbox);
+    taskListElement?.appendChild(taskElement);
+  }
+
+// Load tasks from localStorage
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem('tasks');
+  return storedTasks ? JSON.parse(storedTasks) : [];
 }
+
+// tasks.forEach((task) => renderTask(task));
+tasks.forEach(renderTask);
+
+// Update tasks in localStorage
+function updateStorage(): void {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+

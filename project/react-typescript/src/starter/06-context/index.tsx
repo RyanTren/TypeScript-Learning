37b4dfc226e6cea8816e4bd03fe1f2fea07fbccd
ryan <concepting@protonmail.com>
2024-05-26 13:57:01 -0400

    finished part 6

--- project/react-typescript/src/starter/06-context/index.tsx
+++ project/react-typescript/src/starter/06-context/index.tsx
@@ -1,9 +1,34 @@
+import { useTheme, ThemeProvider } from './context';
+
+function ParentComponent() {
+  return (
+    <ThemeProvider>
+      <Component />
+    </ThemeProvider>
+  );
+  return <Component />;
+}
+
 function Component() {
-    return (
-      <div>
-        <h2>React & Typescript</h2>
-        <h2>Context API</h2>
-      </div>
-    );
-  }
-  export default Component;
+  const context = useTheme();
+  console.log(context);
+
+  return (
+    <div>
+      <h2>random component</h2>
+      <button
+        onClick={() => {
+          if (context.theme === 'dark') {
+            context.setTheme('system');
+            return;
+          }
+          context.setTheme('dark');
+        }}
+        className='btn btn-center'
+      >
+        toggle theme
+      </button>
+    </div>
+  );
+}
+export default ParentComponent;

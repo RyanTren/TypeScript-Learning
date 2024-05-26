b96bd568c6dcef9ad197d9f7373b74f5ec0857da
ryan <concepting@protonmail.com>
2024-05-26 14:30:44 -0400

    lesson 9 react tsx done

--- project/react-typescript/src/main.tsx
+++ project/react-typescript/src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {store} from './store.ts'
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
